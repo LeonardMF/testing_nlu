2823220dba5b99bdb12b5cfa50cdd3d06851dad5
LeonardMF <leonard.fuechsel@gmail.com>
2020-03-27 22:39:51 +0100

    test on differnet device

--- README.md
+++ README.md
@@ -56,6 +56,8 @@ Taken from [spacy.io](https://spacy.io/models/de).
     $ pip freeze > requirements.txt
     $ git add requirements.txt
 
+    $ git install -r requirements.txt
+
 ## Init Rasa
 
     $ rasa init
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # chmod +x entrypoint.sh
 
-#rasa train nlu --nlu ./data/time -c config_time.yml
-rasa train nlu --nlu ./data/vui -c config.yml
+rasa train nlu --nlu ./data/time -c config_time.yml
+#rasa train nlu --nlu ./data/vui -c config.yml
 ## rasa train --data ./data/$case -c config_$case.yml
 rasa run -m models --enable-api --cors '*'
